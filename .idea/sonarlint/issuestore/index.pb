318ed44ca52fcd52b0ff9949b67d70676b67c370
savelyev-an <savelyev_an@mail.ru>
2018-03-19 21:58:33 +0300

    white_box_test_suite

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+X
+(gradle\wrapper\gradle-wrapper.properties,6\9\69215c29c0ba8233881757f77e7ff79a04886148
+[
++src\main\java\team\gp3\new1_ShapeClassifier,7\2\7235368091d990232cacd8aefba825edcb3ca530
+_
+/src\test\java\team\gp3\ShapeClassifierTest.java,d\2\d2dc075e9d68a7e44997290f4667ac6311f76a17
+^
+.src\main\java\team\gp3\old_ShapeClassifier.txt,4\0\40ff1858f5d8a0acf6531dce0bb9691ff6407862
+[
++src\main\java\team\gp3\ShapeClassifier.java,5\9\595d88b89b67259580ee7fb26e99d16b733219ac
--- src/test/java/team/gp3/ShapeClassifierTest.java
+++ src/test/java/team/gp3/ShapeClassifierTest.java
@@ -1,20 +1,126 @@
 package team.gp3;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class ShapeClassifierTest {
 
-    @Before
-    public void setUp() {
-    }
 
-    @After
-    public void tearDown() {
-    }
+        @Rule //https://stackoverflow.com/questions/6141252/dealing-with-system-exit0-in-junit-tests
+        public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
-    @Test
-    public void evaluateGuess() {
+    private static Logger logger = LoggerFactory.getLogger(ShapeClassifierTest.class);
+    String inputArray[]= {
+            "Equilateral,Large,Yes,100,100,100",    //1
+            "Line,Large,Yes,300",    //2
+            "Circle,Large,Yes,500,500",    //3
+            "Rectangle,Large,Yes,500,500,500,500",    //4
+            "Equilateral,Large,Yes,100,200,100",    //5
+            "Equilateral,Small,Yes,20,20,20",    //6
+            "Equilateral,Large,No,99,99,99",    //7
+            "Circle,Large,Yes,300",    //8
+            "Ellipse,Large,Yes,300,500",    //9
+            "Square,Large,Yes,100,500,100,500",    //10
+            "Equilateral,Small,Yes,2,2,2",    //11
+
+            "Equilateral,Large,Yes,100,100,100,4,5",    //13
+
+    };
+
+    String answerArray[]= {
+            "Yes",    //1
+
+
+
+            "Yes",    //2
+            "Yes",    //3
+            "No",    //4
+            "No",    //5
+            "Yes",    //6
+            "Yes",    //7
+            "No",    //8
+            "Yes",    //9
+            "No",    //10
+            "Yes",    //11
+
+            "No"
+
+    };
+
+
+
+        @BeforeClass
+        public static void beforeTest (){
+//        logger.info("this is @BeforeClass method");
+
+        }
+
+        @Before
+        public void before(){
+//        logger.info("this is @Before method");
+        }
+
+
+
+        @org.junit.Test
+        public void singleInputTest  (){
+//        exit.expectSystemExit(); // here should be all OK
+
+            ShapeClassifier shapeClassifier;
+            boolean Ok=true;
+            int failedTests = 0;
+            int passedTests =0;
+            for(int i=0;i<inputArray.length;i++) {
+                shapeClassifier = new ShapeClassifier(); // to have new classfiier
+                String input = inputArray[i];
+                String answer = shapeClassifier.evaluateGuess(input);
+                logger.info("test "+(i+1)+" input= " + input + "  real/expected answer= " + answer+ "/"+
+                        answerArray[i]+ (answerArray[i].equals(answer) ? "" : "   test failed!"));
+                if (answerArray[i].equals(answer)){
+                    passedTests++;
+                } else {
+                    failedTests++;
+                    Ok=false;
+                }
+            }
+            logger.info("tests passed = "+passedTests+" tests failed = "+failedTests);
+            Assert.assertTrue("Every test should be passed", Ok);
+        }
+
+        @org.junit.Test
+        public void tripleWrongInputTest  (){
+            //the code under test, which calls System.exit(0)
+            Assert.assertTrue("Every test should be passed", true);
+
+            logger.info("tripleWrongInputTest Starts ");
+            String wrongInput="Equilateral,Large,Yes,100,200,100";
+            ShapeClassifier shapeClassifier=new ShapeClassifier();
+            String answer=null;
+            for(int i=0;i<2;i++) {
+                answer = shapeClassifier.evaluateGuess(wrongInput);
+                logger.info("test "+(i+1)+" input= " + wrongInput + "answwer = " +answer);
+            }
+
+            exit.expectSystemExitWithStatus(1);
+            answer = shapeClassifier.evaluateGuess(wrongInput);
+            Assert.assertTrue("Every test should be passed", true);
+        }
+
+
+
+        @After
+        public void after(){
+//        logger.info("this is @After method");
+        }
+        @AfterClass
+        public static void afterClass(){
+//        logger.info("this is @AfterClass method");
+
+        }
+
+
     }
-}
